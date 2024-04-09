6dd03f84dc35e7dc6dff248ab7a06af70e548c18
- <->
2024-04-09 10:31:26 -0600

    menu desplegable

---
+++ src/main/java/fernando/morales/menu_despegable/ui/slideshow/SlideshowViewModel.kt
@@ -0,0 +1,13 @@
+package fernando.morales.menu_despegable.ui.slideshow
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class SlideshowViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is slideshow Fragment"
+    }
+    val text: LiveData<String> = _text
+}
